dcd173773dd63dfd02300f7635deaf43f8ce6f00
Asomborac <asomborac@gmail.com>
2019-09-27 22:48:03 +0200

    2

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,41 +5,132 @@ import TextField from "material-ui/TextField";
 import RaisedButton from "material-ui/RaisedButton";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import List from "../components/List";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import { styled } from "@material-ui/styles";
+
+const InputStyle = styled(FormControl)({
+  minWidth: 260
+});
 
 class Form extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      name: "",
       text: "",
-      year: ""
+      year: "",
+      region: "",
+      price: ""
     };
   }
 
   handleChange = e => {
     const { name, value } = e.target;
 
-    this.setState({
-      [name]: value
-    });
+    this.setState({ [name]: value });
   };
 
   handleSubmit = e => {
     e.preventDefault();
-
     let entry = {
+      name: this.state.name,
       text: this.state.text,
-      year: this.state.year
+      year: this.state.year,
+      region: this.state.region,
+      price: this.state.price,
     };
 
     this.props.createEntry(entry);
 
-    this.setState({
-      text: ""
-    });
-    this.setState({
-      year: ""
-    });
+    this.setState({ name: "" });
+    this.setState({ text: "" });
+    this.setState({ year: "" });
+    this.setState({ region: "" });
+    this.setState({ price: "" });
+  }
+
+  handle20 = e => {
+    for (let i=0; i<20; i++){
+      this.handleSubmitRandom(e);
+    }
+  }
+
+  handleSubmitRandom = e => {
+    e.preventDefault();
+
+    let nameif = this.state.name;
+    let textif = this.state.text;
+    let yearif = this.state.year;
+    let regionif = this.state.region;
+    let priceif = this.state.price;
+
+    let wineTypes = [
+      "Chardonnay",
+      "Riesling",
+      "Pinot Grigio",
+      "Sauvignon Blanc",
+      "Cabernet Sauvignon",
+      "Pinot Noir",
+      "Syrah",
+      "Zinfandel",
+      "Malbec",
+      "Merlot"
+    ];
+
+    let wineRegions = [
+      "France",
+      "Italy",
+      "Spain",
+      "United States",
+      "Argentina",
+      "Australia",
+      "Germany",
+      "South Africa",
+      "Chile",
+      "Portugal"
+    ];
+
+    if (nameif === "") {
+      nameif = "Wine";
+    }
+
+    if (textif === "") {
+      textif = wineTypes[Math.floor(Math.random() * wineTypes.length)];
+    }
+
+    if (yearif === "") {
+      yearif = getRandomArbitrary(1960, 2019);
+      yearif = Math.round(yearif);
+    }
+
+    if (regionif === "") {
+      regionif = wineRegions[Math.floor(Math.random() * wineRegions.length)];
+    }
+
+    if (priceif === "") {
+      priceif = getRandomArbitrary(200, 2000);
+      priceif = Math.round(priceif);
+    }
+
+    let entry = {
+      name: nameif,
+      text: textif,
+      year: yearif,
+      region: regionif,
+      price: priceif
+    };
+
+    this.props.createEntry(entry);
+
+    this.setState({ name: "" });
+    this.setState({ text: "" });
+    this.setState({ year: "" });
+    this.setState({ region: "" });
+    this.setState({ price: "" });
   };
 
   handleReset = e => {
@@ -51,19 +142,85 @@ class Form extends Component {
       <MuiThemeProvider>
         <div className="pads">
           <TextField
-            name="text"
-            hintText="Input text here"
-            floatingLabelText="Text entry"
-            value={this.state.text}
+            name="name"
+            hintText="Input wine name here"
+            floatingLabelText="Wine name"
+            value={this.state.name}
             onChange={this.handleChange}
             floatingLabelFixed
           />
+          <br />
+
+          <InputStyle>
+            <InputLabel>Wine type</InputLabel>
+            <Select
+              helperText="Select a wine"
+              value={this.state.text}
+              onChange={this.handleChange}
+              inputProps={{
+                name: "text",
+                id: "text-simple"
+              }}
+            >
+              <MenuItem value="none" disabled>
+                Select a wine
+              </MenuItem>
+              <MenuItem value="Chardonnay">Chardonnay</MenuItem>
+              <MenuItem value="Riesling">Riesling</MenuItem>
+              <MenuItem value="Pinot Grigio">Pinot Grigio</MenuItem>
+              <MenuItem value="Sauvignon Blanc">Sauvignon Blanc</MenuItem>
+              <MenuItem value="Cabernet Sauvignon">Cabernet Sauvignon</MenuItem>
+              <MenuItem value="Pinot Noir">Pinot Noir</MenuItem>
+              <MenuItem value="Syrah">Syrah</MenuItem>
+              <MenuItem value="Zinfandel">Zinfandel</MenuItem>
+              <MenuItem value="Malbec">Malbec</MenuItem>
+              <MenuItem value="Merlot">Merlot</MenuItem>
+            </Select>
+          </InputStyle>
+
+          <br />
+
+          <InputStyle>
+            <InputLabel>Wine Region</InputLabel>
+            <Select
+              helperText="Select a region"
+              value={this.state.region}
+              onChange={this.handleChange}
+              inputProps={{
+                name: "region",
+                id: "region-simple"
+              }}
+            >
+              <MenuItem value="none" disabled>
+                Select a region
+              </MenuItem>
+              <MenuItem value="France">France</MenuItem>
+              <MenuItem value="Italy">Italy</MenuItem>
+              <MenuItem value="Spain">Spain</MenuItem>
+              <MenuItem value="United States">United States</MenuItem>
+              <MenuItem value="Argentina">Argentina</MenuItem>
+              <MenuItem value="Australia">Australia</MenuItem>
+              <MenuItem value="Germany">Germany</MenuItem>
+              <MenuItem value="South Africa">South Africa</MenuItem>
+              <MenuItem value="Chile">Chile</MenuItem>
+              <MenuItem value="Portugal">Portugal</MenuItem>
+            </Select>
+          </InputStyle>
           <br />
           <TextField
             name="year"
             hintText="Input year here"
             floatingLabelText="Year entry"
             value={this.state.year}
+            onChange={this.handleChange}
+            floatingLabelFixed
+          />
+          <br />
+          <TextField
+            name="price"
+            hintText="Input wine price here"
+            floatingLabelText="Wine price"
+            value={this.state.price}
             onChange={this.handleChange}
             floatingLabelFixed
           />
@@ -77,6 +234,18 @@ class Form extends Component {
           <RaisedButton
             label="Reset"
             onClick={e => this.handleReset(e)}
+            primary
+            className="batan"
+          />
+          <RaisedButton
+            label="Random"
+            onClick={e => this.handleSubmitRandom(e)}
+            primary
+            className="batan"
+          />
+            <RaisedButton
+            label="Random 20"
+            onClick={e => this.handle20(e)}
             primary
             className="batan"
           />
@@ -102,3 +271,7 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(Form);
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions/actions";
 import RaisedButton from "material-ui/RaisedButton";
+import Table from "@material-ui/core/Table";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import { styled } from "@material-ui/styles";
+
+const NewCell1 = styled(TableCell)({
+  width: "40px"
+});
+
+const NewCell2 = styled(TableCell)({
+  width: "120px"
+});
+
+const NewCell3 = styled(TableCell)({
+  width: "200px",
+  align: "center"
+});
 
 class List extends Component {
   handleDelete = (e, index) => {
@@ -9,31 +27,43 @@ class List extends Component {
     this.props.deleteEntry(index);
   };
 
-  listView(data, index) {
-    console.log(data);
+  tableBody(data, index) {
     return (
-      <div className="row">
-        <li key={index} className="mylist">
-          {index}-{data.text},{data.year}
+      <TableRow key={index} className="ab">
+        <NewCell1>{index + 1}</NewCell1>
+        <NewCell3>{data.name}</NewCell3>
+        <NewCell3>{data.text}</NewCell3>
+        <NewCell3>{data.region}</NewCell3>
+        <NewCell3>{data.year}</NewCell3>
+        <NewCell3>{data.price}</NewCell3>
+        <NewCell2>
           <RaisedButton
             className="batan"
             label="Delete"
             onClick={e => this.handleDelete(e, index)}
             primary
           />
-        </li>
-      </div>
+        </NewCell2>
+      </TableRow>
     );
   }
 
   render() {
     return (
       <div>
-        {
-          <ul className="list-group">
-            {this.props.entries.map((entry, i) => this.listView(entry, i))}
-          </ul>
-        }
+        <Table>
+          <TableHead>
+            <NewCell1>Id</NewCell1>
+            <NewCell3>Name</NewCell3>
+            <NewCell3>Type</NewCell3>
+            <NewCell3>Region</NewCell3>
+            <NewCell3>Year</NewCell3>
+            <NewCell3>Price</NewCell3>
+            <NewCell2>        Delete</NewCell2>
+          </TableHead>
+        </Table>
+
+        {this.props.entries.map((entry, i) => this.tableBody(entry, i))}
       </div>
     );
   }
